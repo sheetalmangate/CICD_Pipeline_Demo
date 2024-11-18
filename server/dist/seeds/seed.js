9ce900592805ef8bae5b9306d90f0a96d6fbffd0
Sheetal Mangate <sheetal.mangate@gmail.com>
2024-11-18 13:43:46 -0800

    reverting seed to original code

--- server/dist/seeds/seed.js
+++ server/dist/seeds/seed.js
@@ -1,7 +1,7 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import pythonQuestions from './pythonQuestions.js';
+import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
 db.once('open', async () => {
     await cleanDB('Question', 'questions');
     await Question.insertMany(pythonQuestions);
